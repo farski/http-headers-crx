8d811e0c07f0dec6c3d324307ddbf3d9296d2be0
Chris Kalafarski <chris@farski.com>
2017-05-17 09:36:21 -0400

    Add browser action badge support

--- scripts/background.js
+++ scripts/background.js
@@ -28,6 +28,7 @@ const txExtra = ['requestHeaders'];
 
 function rxCallback(details) {
   _store[details.tabId].push(details);
+  updateBadge(details.tabId);
 }
 
 function txCallback(details) {
@@ -39,6 +40,39 @@ function txCallback(details) {
   _store[details.tabId].push(details);
 }
 
+function updateBadge(tabId) {
+  const details = _store[tabId];
+  const lastDetails = details[details.length - 1];
+
+  let color = '#3B3B3B';
+
+  if (lastDetails.statusCode >= 500) {
+    color = '#BA0000';
+  } else if (lastDetails.statusCode >= 400) {
+    color = '#DE5500';
+  } else if (lastDetails.statusCode >= 300) {
+    color = '#0062A3';
+  } else if (lastDetails.statusCode >= 200) {
+    color = '#078F00';
+  }
+
+  let text = '•';
+  if (lastDetails.statusCode) { text = `${lastDetails.statusCode}` }
+
+  // If there's more than two details, it means a single request and response
+  // wasn't enough; probably a redirect. Indicate how many total roundtrips were
+  // needed
+  if (details.length > 2) {
+    text = `× ${Math.round(details.length / 2)}`;
+  }
+
+  chrome.browserAction.setBadgeBackgroundColor({color: color, tabId: tabId});
+  chrome.browserAction.setBadgeText({text: text, tabId: tabId});
+}
+
 chrome.webRequest.onHeadersReceived.addListener(rxCallback, filter, rxExtra);
 chrome.webRequest.onSendHeaders.addListener(txCallback, filter, txExtra);
+
+chrome.tabs.onUpdated.addListener(tabId => updateBadge(tabId));
+chrome.tabs.onActivated.addListener(info => updateBadge(info.tabId));
 chrome.tabs.onRemoved.addListener(tabId => delete(_store[tabId]));
